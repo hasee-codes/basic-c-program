dd2865702183739380a64ec26738e0b977837d7b
hasee-codes <haseenasoultane2008@gmail.com>
2026-08-25 10:18:30 +0530

    Add palindrome checker in Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a Palindrome", original);
+    else
+        printf("%d is Not a Palindrome", original);
+
+    return 0;
+}
